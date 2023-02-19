590d1cf30b3d87ee26e9ce2155b991d6a4128aab
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-02-19 13:05:03 +0300

    Довел прохождение тестов до 100% на unittest

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -7,11 +7,22 @@ class TestArrs(unittest.TestCase):
     def test_get_normal(self):
         self.assertEqual(arrs.get([1, 2, 3], 2, "test"), 3)
 
+    def test_get_negative_index(self):
+        self.assertEqual(arrs.get([1, 2, 3], -2, "test"), "test")
+
     def test_get_IndexError(self):
         with self.assertRaises(IndexError):
             arrs.get([], 0, "test"), "test"
 
-
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+
+    def test_slice_zero_length(self):
+        self.assertEqual(arrs.my_slice([]), [])
+
+    def test_slice_negative_length(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3], -4, -2), [1])
+
+    def test_slice_normalazied_start(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3], -2), [2, 3])
